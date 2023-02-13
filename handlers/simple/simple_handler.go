acbc339619910a1630cadbe04bd032462017f4f3
Jonathan Amsterdam <jba@google.com>
2023-02-13 16:24:20 -0500

    handlers/simple: easy way to write a Handler

---
+++ handlers/simple/simple_handler.go
@@ -0,0 +1,55 @@
+package simple
+
+import (
+	"context"
+
+	"github.com/jba/slog/withsupport"
+	"golang.org/x/exp/slices"
+	"golang.org/x/exp/slog"
+)
+
+func Handler(handle func(slog.Record) error, opts slog.HandlerOptions) slog.Handler {
+	return &simpleHandler{opts, handle, nil}
+}
+
+type simpleHandler struct {
+	opts   slog.HandlerOptions
+	handle func(slog.Record) error
+	goa    *withsupport.GroupOrAttrs
+}
+
+func (h *simpleHandler) Enabled(ctx context.Context, l slog.Level) bool {
+	minLevel := slog.LevelInfo
+	if h.opts.Level != nil {
+		minLevel = h.opts.Level.Level()
+	}
+	return l >= minLevel
+}
+
+func (h *simpleHandler) WithGroup(name string) slog.Handler {
+	h2 := *h
+	h2.goa = h2.goa.WithGroup(name)
+	return &h2
+}
+
+func (h *simpleHandler) WithAttrs(as []slog.Attr) slog.Handler {
+	h2 := *h
+	h2.goa = h2.goa.WithAttrs(as)
+	return &h2
+}
+
+func (h *simpleHandler) Handle(r slog.Record) error {
+	r2 := slog.NewRecord(r.Time, r.Level, r.Message, r.PC, r.Context)
+
+	var attrs []slog.Attr
+	r.Attrs(func(a slog.Attr) { attrs = append(attrs, a) })
+	for g := h.goa; g != nil; g = g.Next {
+		if g.Group != "" {
+			attrs = []slog.Attr{slog.Group(g.Group, attrs...)}
+		} else {
+			attrs = append(slices.Clip(g.Attrs), attrs...)
+		}
+	}
+	r2.AddAttrs(attrs...)
+	return h.handle(r2)
+}
---
+++ handlers/simple/simple_handler_test.go
@@ -0,0 +1,61 @@
+package simple
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+
+	"golang.org/x/exp/slog"
+)
+
+func newHandle(w io.Writer) func(slog.Record) error {
+	return func(r slog.Record) (err error) {
+		fmt.Fprintf(w, "level=%s", r.Level)
+		fmt.Fprintf(w, " msg=%q", r.Message)
+		r.Attrs(func(a slog.Attr) {
+			if err == nil {
+				err = formatAttr(w, a)
+			}
+		})
+		return err
+	}
+}
+
+func formatAttr(w io.Writer, a slog.Attr) error {
+	if a.Value.Kind() == slog.KindGroup {
+		gs := a.Value.Group()
+		if len(gs) == 0 {
+			return nil
+		}
+		if a.Key != "" {
+			fmt.Fprintf(w, " (%s)", a.Key)
+		}
+		for _, g := range gs {
+			if err := formatAttr(w, g); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	if a.Key == "" {
+		return nil
+	}
+	_, err := fmt.Fprintf(w, " %s=%s", a.Key, a.Value)
+	return err
+}
+
+func Test(t *testing.T) {
+	var buf bytes.Buffer
+	logger := slog.New(Handler(newHandle(&buf), slog.HandlerOptions{}))
+	logger.With("a", 1).
+		WithGroup("G").
+		With("b", 2).
+		WithGroup("H").
+		Info("msg", "c", 3)
+	got := buf.String()
+	want := `level=INFO msg="msg" a=1 (G) b=2 (H) c=3`
+	if got != want {
+		t.Errorf("got\n%s\nwant\n%s", got, want)
+	}
+}
